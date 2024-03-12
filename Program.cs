6cd90e410d0cc408b2186e0c74968670ebdf2ff6
LilyLavender <lilylambda136@gmail.com>
2024-03-12 13:47:29 -0500

    Rewrote AddToCsv function for new classes

--- Program.cs
+++ Program.cs
@@ -7,11 +7,11 @@ namespace SchallyLilyTicketingSystem
         static void Main(string[] args)
         {
             string filePath = "Tickets.csv";
-            TicketFile ticketFile = new TicketFile(filePath);
             if (!System.IO.File.Exists(filePath)) {
                 Console.WriteLine("Tickets.csv does not exist, please add a valid Tickets.csv first");
                 Environment.Exit(1);
             }
+            TicketFile ticketFile = new TicketFile(filePath);
 
             while (true) {
                 Console.WriteLine("Type 1 to view csv");
@@ -46,33 +46,28 @@ namespace SchallyLilyTicketingSystem
         }
 
         static void AddToCsv(string filePath, TicketFile ticketFile) {
-            /*
-            if (System.IO.File.Exists(filePath))
-            {
-                // Add line header (id, summary, status, etc) to headers array
-                StreamReader sr = new StreamReader(filePath);
-                String[] headers = new String[File.ReadLines(filePath).Count()];
-                for (int i = 0; i < headers.Length; i++)
-                {
-                    string? line = sr.ReadLine();
-                    headers[i] = line.Substring(0, line.IndexOf(","));
+            
+            Ticket ticket = new Ticket();
+            // Print questions for user
+            ticket.summary = GetString("Enter the ticket's summary > ", "Summary cannot be blank");
+            ticket.status = GetString("Enter the ticket's status > ", "Status cannot be blank");
+            ticket.priority = GetString("Enter the ticket's priority > ", "Priority cannot be blank");
+            ticket.submitter = GetString("Enter the ticket's submitter > ", "Submitter cannot be blank");
+            ticket.assigned = GetString("Enter who the ticket is for > ", "Assignee cannot be blank");
+            String input = "";
+            do {
+                input = GetString("Enter a watcher of the ticket, and \"done\" when done > ", "Watcher cannot be blank");
+                if (input != "done") {
+                    ticket.watching.Add(input);
                 }
-                sr.Close();
-                
-                // Print question for user
-                string[] fullFile = File.ReadAllLines(filePath);
-                //fullFile[0] += "," + GenerateNewID(filePath);
-                for (int i = 1; i < headers.Length; i++) {
-                    fullFile[i] += $",{GetString($"Enter your \"{headers[i]}\" value > ", $"\"{headers[i]}\" value cannot be blank")}";
-                }
-                // Add to csv
-                File.WriteAllLines(filePath, fullFile);
-
-                Console.WriteLine("Entry added to Tickets.csv\n");
-            } else {
-                Console.WriteLine("Tickets.csv does not exist!");
+            } while (input != "done");
+            // No watchers
+            if (ticket.watching.Count == 0) {
+                ticket.watching.Add("(no watchers)");
             }
-            */
+
+            // Add ticket
+            ticketFile.AddTicket(ticket);
         }
 
         static int GetInt(bool restrictValues, int intMin, int intMax, string prompt, string errorMsg) {

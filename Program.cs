59895231c60b097d0cbe1f768893d90e2e1b4a04
LilyLavender <lilylambda136@gmail.com>
2024-03-12 09:39:57 -0500

    Various readability fixes

--- Program.cs
+++ Program.cs
@@ -13,10 +13,10 @@ namespace SchallyLilyTicketingSystem
                 int option = GetInt(true, 0, 2, "", "Number must be one of the aforementioned values");
                 switch(option) {
                     case 1:
-                        viewCsv();
+                        ViewCsv();
                         break;
                     case 2:
-                        addToCsv();
+                        AddToCsv();
                         break;
                     default:
                         Environment.Exit(1);
@@ -25,19 +25,21 @@ namespace SchallyLilyTicketingSystem
             }
         }
 
-        static void viewCsv() {
+        static void ViewCsv() {
             string file = "Tickets.csv";
             if (System.IO.File.Exists(file))
             {
+                // Add data to fullCsv array
                 StreamReader sr = new StreamReader(file);
                 String[][] fullCsv = new String[File.ReadLines(file).Count()][];
                 for (int i = 0; i < fullCsv.Length; i++)
                 {
-                    string line = sr.ReadLine();
+                    string? line = sr.ReadLine();
                     fullCsv[i] = line.Split(',');
                 }
                 sr.Close();
 
+                // Print from fullCsv array
                 for (int j = 0; j < fullCsv[0].Length; j++) {
                     for (int i = 0; i < fullCsv.Length; i++) {
                         Console.Write($"{fullCsv[i][j],-21}");
@@ -50,23 +52,26 @@ namespace SchallyLilyTicketingSystem
             }
         }
 
-        static void addToCsv() {
+        static void AddToCsv() {
             string file = "Tickets.csv";
             if (System.IO.File.Exists(file))
             {
+                // Add line header (id, summary, status, etc) to headers array
                 StreamReader sr = new StreamReader(file);
                 String[] headers = new String[File.ReadLines(file).Count()];
                 for (int i = 0; i < headers.Length; i++)
                 {
-                    string line = sr.ReadLine();
+                    string? line = sr.ReadLine();
                     headers[i] = line.Substring(0, line.IndexOf(","));
                 }
                 sr.Close();
                 
+                // Print question for user
                 string[] fullFile = File.ReadAllLines(file);
                 for (int i = 0; i < headers.Length; i++) {
                     fullFile[i] += $",{GetString($"Enter your \"{headers[i]}\" value > ", $"\"{headers[i]}\" value cannot be blank")}";
                 }
+                // Add to csv
                 File.WriteAllLines(file, fullFile);
 
                 Console.WriteLine("Entry added to Tickets.csv\n");
